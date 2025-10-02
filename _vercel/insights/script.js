4fe72ed8898a364d5481be49fb151c71c0fa68bf
usman82921 <usman05054@gmail.com>
2025-10-02 06:33:52 +0500

    Create script.js

---
+++ _vercel/insights/script.js
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/png" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Reward System - Claim Daily Rewards</title>
+    <meta name="title" content="Reward System - Claim Daily Rewards" />
+    <meta 
+      name="description" 
+      content="Log in to the Reward System to claim your daily rewards, track points, and access exclusive offers. Your one-stop platform for managing all your loyalty benefits." 
+    />
+    <meta 
+      name="keywords" 
+      content="rewards, daily rewards, claim points, loyalty program, customer rewards, exclusive offers" 
+    />
+    <meta name="author" content="Talha Riaz" />
+    <link rel="canonical" href="https://ufone.talhablogging.site/" /> <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://ufone.talhablogging.site/" /> <meta property="og:title" content="Reward System - Claim Daily Rewards" />
+    <meta property="og:description" content="Log in to claim your daily rewards, track points, and access exclusive offers." />
+    <meta property="og:image" content="https://ufone.talhablogging.site/social-preview-image.png" /> <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://ufone.talhablogging.site/" /> <meta property="twitter:title" content="Reward System - Claim Daily Rewards" />
+    <meta property="twitter:description" content="Log in to claim your daily rewards, track points, and access exclusive offers." />
+    <meta property="twitter:image" content="https://ufone.talhablogging.site/social-preview-image.png" />   <script type="module" crossorigin src="/assets/index-CqT8fBjE.js"></script>
+   <link rel="stylesheet" crossorigin href="/assets/index-Bmk2QQoT.css">
+ </head>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5705856509590868"
+     crossorigin="anonymous"></script>
+  </body>
+</html>
